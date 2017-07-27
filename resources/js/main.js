e5e657091548bbee9e96b95a2a6a0996fc36f9c1
Zia <zia_gt@yahoo.com>
2017-07-27 02:00:00 +0200

    Goto Top button added

--- resources/js/main.js
+++ resources/js/main.js
@@ -55,19 +55,18 @@ $(document).ready(function(){
     });
 });
 
-// When the user scrolls down 20px from the top of the document, show the button
+/* GOTO TOP BUTTON START */
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("gotoTop").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("gotoTop").style.display = "none";
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+/* GOTO TOP BUTTON END */
